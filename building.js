0ce7a6e17f7be6e11684d0c53355cb80e0a90af4
adonald403 <59670140+adonald403@users.noreply.github.com>
2020-03-04 09:15:50 -0600

    Init

---
+++ building.js
@@ -0,0 +1,305 @@
+Building = Ice.$extend('Building', {
+    __init__: function(opts) {
+        var self = this;
+        self.$super();
+        self.tier = ko.observable(opts.tier || 1);
+        self.kind = ko.observable(opts.kind || 'Shacks');
+        self.name = ko.observable(opts.name || (BUILDING_NAMES[self.kind() + '.' + self.tier()]));
+
+        self.unlocked = ko.observable(opts.unlocked || false);
+        self.purchased = ko.observable(opts.qty || 0);
+        self.qty = ko.observable(opts.qty || 0);
+        self.cost = ko.observable(opts.cost || Math.pow(100, self.tier()));
+        self.proof_cost = ko.observable(opts.proof_cost || 0);
+
+        self.upgrade_currency = ko.observable(opts.upgrade_currency || 'money');
+        self.upgrade_cost = ko.observable(opts.upgrade_cost || Math.pow(1000000, self.tier()-1));
+        self.upgrade_bonus = ko.observable(1);
+
+        self.integrates_to = ko.observable(0);
+
+        self.cost_factor = ko.observable(opts.cost_factor || (1 + 0.05 * self.tier()));
+        self.proof_cost_factor = ko.observable(opts.proof_cost_factor || 1.1);
+
+        self.per_tick = ko.observable(opts.per_tick || {});
+        self.per_click = ko.observable(opts.per_click || {});
+
+    },
+    __keys__: function() {
+        return this.$super().concat([
+            'qty', 'purchased',
+            'cost', 'proof_cost',
+            'cost_factor', 'proof_cost_factor',
+            'per_tick', 'per_click'
+        ]);
+    },
+    key: Ice.kocomputed(function() {
+        var self = this;
+        return self.kind() + '.' + self.tier();
+    }),
+
+    buy: function(amt) {
+        var self = this;
+
+        if(!self.unlocked()) {
+            return;
+        }
+
+        // I'm thorough.
+        if(!amt || amt < 1) {
+            amt = 1;
+        }
+        amt = Math.floor(amt);
+
+         //Reduce rendering overhead.
+
+        var money = game.money();
+        var bugs = game.bugs();
+        var cost = self.cost();
+        var proof_cost = self.proof_cost();
+        var qty = self.qty();
+
+        while(amt >= 1) {
+
+            if(money < cost || bugs < proof_cost) {
+                console.log("Cannot afford more.");
+                return;
+            }
+
+            money -= self.cost();
+            bugs -= self.proof_cost();
+
+            cost *= self.cost_factor();
+            proof_cost *= self.proof_cost_factor();
+
+            qty += 1;
+            amt -= 1;
+        }
+
+        game.money(money);
+        game.bugs(bugs);
+        self.qty(qty);
+        self.cost(cost);
+        self.proof_cost(proof_cost);
+    },
+    css: function() {
+        var self = this;
+
+        var classes = "";
+        if(self.upgrade_cost() < game.money()) {
+            classes += ' upgrade_ready';
+        }
+        if(self.can_integrate()) {
+            classes += ' integration_ready';
+        }
+        return classes;
+    },
+    can_upgrade: Ice.kocomputed(function() {
+        if(!lazy_game()) return;
+        var self = this;
+        return self.upgrade_cost() < game[self.upgrade_currency()]();
+    }),
+    upgrade: function() {
+        var self = this;
+        var upgrade_cost_factor = 10;
+        var cash_obs = game[self.upgrade_currency()];
+        if(cash_obs() < self.upgrade_cost()) {
+            return;
+        }
+        cash_obs(cash_obs() - self.upgrade_cost());
+        self.upgrade_cost(self.upgrade_cost() * upgrade_cost_factor);
+        self.upgrade_bonus(self.upgrade_bonus() + game.upgrade_effectiveness());
+
+        game.throttled_save();
+    },
+    tick: function() {
+        var self = this;
+        _.each(self.per_tick(), function(amt, currency) {
+            var mult = self.get_multiplier('tick', currency);
+            if(currency === 'money' || currency === 'bugs') {
+                game[currency](game[currency]() + mult * amt * self.qty());
+            } else {
+                var bld = game.indexed_buildings()[currency];
+                bld.qty(bld.qty() + mult * amt * self.qty());
+            }
+        });
+
+    },
+    click: function() {
+        var self = this;
+        _.each(self.per_click(), function(amt, currency) {
+            var mult = self.get_multiplier('tick', currency);
+            if(currency === 'money' || currency === 'bugs') {
+                game[currency](game[currency]() + mult * amt * self.qty());
+            } else {
+                var bld = game.indexed_buildings()[currency];
+                bld.qty(bld.qty() + mult * amt * self.qty());
+            }
+        });
+
+    },
+    get_multiplier: function(mode, currency) {
+        var self = this;
+        if(self.kind == 'QA') {
+            return self.upgrade_bonus();
+        } else {
+            var qa = game.indexed_buildings()['QA.' + self.tier()];
+            return self.upgrade_bonus() * qa.qa_bonus();
+        }
+    },
+    qa_bonus: function() {
+        var self = this;
+
+        if(self.qty() === 0) {
+            return 1;
+        }
+        return 1 + Math.log(self.qty() * self.upgrade_bonus()) / log10;
+    },
+    programmer_click_power: function() {
+        var self = this;
+        if(!self.qty()) { return 0; }
+        return Math.floor(Math.log(self.qty()) / log100);
+    },
+    programmer_autoclicks_per_tick: function() {
+        var self = this;
+        if(!self.qty()) { return 0; }
+        return Math.floor(Math.log(self.qty()) / log100);
+    },
+    ifactor: Ice.kocomputed(function() {
+        var self = this;
+        return Math.pow(100, self.tier());
+
+    }),
+    integrate: function() {
+        var self = this;
+        var ifactor = self.ifactor();
+        var next = self.next;
+
+
+        if(!next) {
+            return; // Or make a new one, later.
+        }
+        var will_integrate = self.integration_count(self.qty());
+        if(will_integrate === 0) return;
+
+        self.qty(0);
+
+        next.qty(next.qty() + will_integrate);
+        next.unlocked(true);
+
+
+    },
+    integration_count: function(qty) {
+        var self = this;
+        var ifactor = self.ifactor();
+
+        if(!self.next) {
+            return 0;
+        }
+
+        return Math.floor(qty / ifactor);
+    },
+    can_integrate: function() {
+        var self = this;
+        if(!self.next) {
+            return self.integrates_to() > 0;
+        }
+        return self.integrates_to() > 0 && self.next.integrates_to() === 0;
+    }
+});
+
+BUILDINGS = [
+    {
+        kind: 'IT',
+        tier: 1,
+        per_click: {'money': 1},
+    },
+    {
+        kind: 'QA',
+        tier: 1,
+        upgrade_currency: 'bugs',
+    },
+    {
+        kind: 'Programmer',
+        tier: 1,
+        upgrade_currency: 'bugs',
+    },
+    {
+        kind: 'DB',
+        tier:1,
+        per_tick: {'money': 10},
+    },
+    {
+        kind: 'User',
+        tier: 1,
+        per_tick: {'bugs': 1},
+    }
+];
+
+_.each(['IT', 'QA', 'Programmer', 'DB', 'User'], function(kind) {
+    for(var x=2;x<=8;x++) {
+        var bld = {
+            kind: kind,
+            tier: x,
+            per_click: {},
+            per_tick: {},
+            upgrade_currency: kind === 'Programmer' || kind === 'QA' ? 'bugs' : 'money'
+        };
+        var prevkey = kind + '.' + (x-1);
+        if(kind === 'IT') {
+            bld.per_click[prevkey] = 1;
+        } else {
+            bld.per_tick[prevkey] = 1;
+        }
+        BUILDINGS.push(bld);
+    }
+});
+
+BUILDING_NAMES = {
+    'IT.1': 'Nephew',
+    'IT.2': 'Rent-A-Geek',
+    'IT.3': 'Manuals',
+    'IT.4': 'Help Desk',
+    'IT.5': 'IT Consultant',
+    'IT.6': 'Server Admin',
+    'IT.7': 'Cloud Architect',
+    'IT.8': 'Tim Berners-Lee',
+
+    'QA.1': 'Red Pens',
+    'QA.2': 'Print Preview',
+    'QA.3': 'Frequent Backups',
+    'QA.4': 'Peer Review',
+    'QA.5': 'CVS',
+    'QA.6': 'SVN',
+    'QA.7': 'Hg',
+    'QA.8': 'Git',
+
+    'Programmer.1': 'Will Wright',
+    'Programmer.2': 'Richard Bartle',
+    'Programmer.3': 'Peter Molyneux',
+    'Programmer.4': 'Bjarne Strossup',
+    'Programmer.5': 'Guido Van Rossum',
+    'Programmer.6': 'Grace Hopper',
+    'Programmer.7': 'Ada Lovelace',
+    'Programmer.8': 'Alan Turing',
+
+    'DB.1': 'Trapper Keeper',
+    'DB.2': 'BDB',
+    'DB.3': 'SQLite',
+    'DB.4': 'Oracle',
+    'DB.5': 'NoSQL',
+    'DB.6': 'MySQL',
+    'DB.7': 'Postgres',
+    'DB.8': 'TSQL',
+
+    'User.1': 'YouTube User',
+    'User.2': 'Trained Monkey',
+    'User.3': 'Forum Troll',
+    'User.4': '"Enthusiast"',
+    'User.5': 'John Q. Public',
+    'User.6': 'OEM',
+    'User.7': 'Casual Debugger',
+    'User.8': 'Chaos Monkey'
+
+
+};
---
+++ main.js
@@ -0,0 +1,367 @@
+$(function(){
+    /* https://gist.github.com/AndreasBriese/1670507 */
+    _.mixin({
+        sum: function(obj, iterator, context) {
+        if (!iterator && _.isEmpty(obj)) return 0;
+        var result = 0;
+        if (!iterator && _.isArray(obj)){
+            for(var i=obj.length-1;i>-1;i-=1){
+                result += obj[i];
+            }
+          return result;
+        }
+        _.each(obj, function(value, index, list) {
+            var computed = iterator ? iterator.call(context, value, index, list) : value;
+            result += computed;
+        });
+        return result;
+      }
+    });
+
+    ShowFullErrorStacks();
+    MonkeypatchKoTemplateBinding();
+
+    loadExternalKnockoutTemplates('/kot/', function() {
+
+
+        sound_manifest = [
+        ];
+
+        createjs.Sound.registerManifest(sound_manifest);
+
+        settings = new Settings();
+        settings.load();
+
+
+        lazy_game = ko.observable(null);
+        game = Second.start_game();
+        lazy_game(game);
+
+        var body = $('#game');
+
+        ko.applyBindings( window.game, body[0]);
+
+
+    });
+
+
+});
+
+log10 = Math.log(10);
+log100 = Math.log(100);
+log1000 = Math.log(1000);
+NUMBER_SUFFIXES = ["K", "M", "B", "T", "Qa", "Qt", "Sx", "Sp", "Oc", "Nn",
+                   "Dc", "UDc", "DDc", "TDc", "QaDc", "QtDc", "SxDc", "SpDc", "ODc", "NDc",
+                   "Vi", "UVi", "DVi", "TVi", "QaVi", "QtVi", "SxVi", "SpVi", "OcVi", "NnVi",
+                   "Tg", "UTg", "DTg", "TTg", "QaTg", "QtTg", "SxTg", "SpTg", "OcTg", "NnTg",
+                   "Qd", "UQd", "DQd", "TQd", "QaQd", "QtQd", "SxQd", "SpQd", "OcQd", "NnQd",
+                   "Qq", "UQq", "DQq", "TQq", "QaQq", "QtQq", "SxQq", "SpQq", "OcQq", "NnQq",
+                   "Sg"
+];
+function format_number(num) {
+    if(num < 1000) {
+        return Math.floor(num);
+    }
+    var digits = Math.floor(Math.log(num) / log10 + 0.1);
+    var suffix = NUMBER_SUFFIXES[Math.floor(digits / 3)-1];
+    var smaller = (num / Math.pow(10, Math.floor(digits/3)*3));
+    var fmted;
+    if(smaller >= 100) fmted = smaller.toFixed(0);
+    else if(smaller >= 10) fmted = smaller.toFixed(1);
+    else fmted = smaller.toFixed(2);
+    return fmted + suffix;
+
+
+}
+
+
+
+Second = Ice.$extend('Second', {
+    __init__: function(blob) {
+        var self = this;
+
+        self.$super();
+        self.buildings = ko.observableArray([]);
+        self.money = ko.observable(100);
+        self.bugs = ko.observable(0);
+        self.upgrade_effectiveness = ko.observable(1);
+        self.total_clicks = ko.observable(0);
+        self.total_ticks = ko.observable(0);
+
+
+        self.load_game(blob);
+        self.tick_interval = window.setInterval(_.bind(self.tick, self), 500);
+        self.refresh_interval = window.setInterval(_.bind(self.refresh_integrate_display, self), 500);
+        self.autosave_interval = window.setInterval(_.bind(self.save_game, self), 60000);
+        self.autoclickinterval = window.setInterval(_.bind(self.autoclick, self), 1000);
+
+
+    },
+    indexed_buildings: Ice.kocomputed(function() {
+        var self = this;
+        return _.indexBy(self.buildings(), function(bld) {
+            return bld.key();
+        });
+    }),
+    buildings_by_tier: Ice.kocomputed(function() {
+        var self = this;
+        return _.sortBy(self.buildings(), function(bld) {
+            return 1 * bld.tier();
+        });
+    }),
+    button_click: function(num) {
+        var self = this;
+        if(!num) {
+            var prog = self.indexed_buildings()['Programmer.1'];
+            num = prog.programmer_click_power();
+        }
+        self.total_clicks(self.total_clicks() + num);
+        click_each = function(bld) { bld.click(); };
+
+        for(var x=0;x<num;x++) {
+            _.each(self.buildings_by_tier(), click_each);
+        }
+    },
+    tick: function() {
+        var self = this;
+        self.total_ticks(self.total_ticks() + 1);
+        _.each(self.buildings_by_tier(), function(bld) {
+            bld.tick();
+        });
+
+
+    },
+    autoclick: function() {
+        var self = this;
+
+        var prog = self.indexed_buildings()['Programmer.1'];
+        //This got exponential REAL fast.
+        //var autoclicks = prog.programmer_autoclicks_per_tick() * prog.programmer_click_power();
+        var autoclicks = prog.programmer_autoclicks_per_tick();
+        if(autoclicks) {
+            self.button_click(autoclicks);
+        }
+    },
+    integrate: function(target) {
+        var self = this;
+        _.each(self.buildings_by_tier(), function(bld) {
+            if(target && bld.tier() >= target.tier()) {
+                return;
+            }
+            if(target && bld.kind() != target.kind()) {
+                return;
+            }
+            bld.integrate();
+        });
+        self.refresh_integrate_display();
+        game.throttled_save();
+    },
+    refresh_building_links: function() {
+        var self = this;
+        _.each(self.buildings(), function(bld) {
+            bld.prev = self.indexed_buildings()[bld.kind() + '.' + (bld.tier()-1)];
+            bld.next = self.indexed_buildings()[bld.kind() + '.' + (bld.tier()+1)];
+        });
+    },
+    kinds: Ice.kocomputed(function() {
+        var self = this;
+        if(!self.buildings().length) {
+            return [];
+        }
+        return _.uniq(_.map(self.buildings(), function(bld) {
+            return bld.kind();
+        }));
+    }),
+    tiers: Ice.kocomputed(function() {
+        var self = this;
+        if(!self.buildings().length) {
+            return 0;
+        }
+        return _.range(1, _.max(self.buildings(), function(b) {
+                                return b.tier();
+                            }).tier() + 1
+        );
+    }),
+
+    refresh_integrate_display: function() {
+        var self = this;
+        _.each(self.kinds(), function(kind) {
+            _.each(self.tiers(), function(tier) {
+                var bld = self.indexed_buildings()[kind + '.' + tier];
+                if(!bld) return;
+                if(!bld.prev) {
+                    bld.integrates_to(0);
+                    return;
+                }
+
+                var willbuild = bld.prev.integration_count(bld.prev.qty() + bld.prev.integrates_to());
+                if(willbuild) {
+                    //bld.prev.integrates_to(0);
+                }
+
+                bld.integrates_to(willbuild);
+
+
+            });
+        });
+    },
+    can_prestige: Ice.kocomputed(function() {
+        var self = this;
+        if(!self.buildings) { return false; }
+        return !!_.sum(self.buildings(), function(bld) {
+            return bld.tier() >= 4 ? bld.qty() : 0;
+        });
+    }),
+    prestige_preview: Ice.kocomputed(function() {
+        var self = this;
+
+        if(!self.can_prestige()) {
+            return 0;
+        }
+        var tiers_reached = {};
+        _.each(self.buildings(), function(bld) {
+            if(!bld.qty() || bld.tier() < 4) return;
+            if(!tiers_reached[bld.kind()] || tiers_reached[bld.kind()] < bld.tier()) {
+                tiers_reached[bld.kind()] = bld.tier();
+            }
+        });
+
+        return _.sum(tiers_reached, function(tier) {
+            return Math.pow(tier-3, 2) * 0.01;
+        });
+    }),
+
+
+    throttled_save: function() {
+        var self = this;
+        _.debounce(_.bind(self.save_game, self), 1000)();
+    },
+    save_game: function() {
+        var self = this;
+
+        var blob = {
+            building_qtys: {},
+            money: self.money(),
+            bugs: self.bugs(),
+            upgrade_effectiveness: self.upgrade_effectiveness(),
+            total_ticks: self.total_ticks(),
+            total_clicks: self.total_clicks(),
+
+            buildings: {}
+        };
+        _.each(self.buildings(), function(bld) {
+            blob.buildings[bld.key()] = {
+                qty: bld.qty(),
+                unlocked: bld.unlocked(),
+                upgrade_cost: bld.upgrade_cost(),
+                upgrade_bonus: bld.upgrade_bonus()
+            };
+        });
+
+        var json = JSON.stringify(blob);
+        localStorage['Second.current_game'] = json;
+        return blob;
+    },
+    load_game: function(blob) {
+        var self = this;
+        var new_buildings = [];
+        _.each(BUILDINGS, function(bld) {
+            new_buildings.push(Building(bld));
+        });
+        self.buildings(new_buildings);
+        self.refresh_building_links();
+
+        _.each(['money', 'bugs', 'upgrade_effectiveness', 'total_clicks', 'total_ticks'], function(attr) {
+            self[attr](blob[attr]);
+        });
+
+        _.each(blob.buildings || {}, function(bld_blob, key) {
+            var bld = self.indexed_buildings()[key];
+            if(!bld) return;
+            _.each(['qty', 'upgrade_cost', 'upgrade_bonus', 'unlocked'], function(attr) {
+                if(bld_blob[attr] !== undefined) {
+                    bld[attr](bld_blob[attr]);
+                }
+            });
+        });
+
+    },
+    new_game_plus: function() {
+        var self = this;
+        if(!self.can_prestige()) return;
+
+        var blob = JSON.parse(JSON.stringify(Second.new_game_blob));
+        blob.total_clicks = self.total_clicks();
+        blob.total_ticks = self.total_ticks();
+        blob.upgrade_effectiveness = self.upgrade_effectiveness() + self.prestige_preview();
+
+        this.load_game(blob);
+
+    },
+    wipe_save: function() {
+        var self = this;
+        if(!window.confirm("Are you sure?  This IS NOT PRESTIGE.  You're going to lose everything.  Press OK to Prestige, Cancel to stop.")) {
+            return;
+        }
+        if(!window.confirm("I can't help it.  Are you REALLY SURE you want to LOSE ALL PROGRESS and START OVER?  Press OK to start from scratch, Cancel to keep going.")) {
+            return;
+        }
+        self.load_game(Second.new_game_blob);
+    },
+
+});
+
+
+Second.new_game_blob = {
+    money: 0,
+    bugs: 0,
+    upgrade_effectiveness: 1,
+    total_ticks: 0,
+    total_clicks: 0,
+    buildings: {
+        'IT.1': {
+            unlocked: true
+        },
+        'QA.1': {
+            unlocked: true
+        },
+        'Programmer.1': {
+            unlocked: true
+        },
+        'DB.1': {
+            unlocked: true
+        },
+        'User.1': {
+            unlocked: true
+        },
+
+        'IT.2': {
+            qty: 1
+        },
+        'QA.2': {
+            qty:1
+        },
+        'Programmer.2': {
+            qty: 1
+        },
+        'DB.2': {
+            qty: 1
+        },
+        'User.2': {
+            qty: 1
+        }
+    }
+};
+
+Second.start_game = function() {
+    var json = localStorage['Second.current_game'];
+    var blob;
+    if(json) {
+        blob = JSON.parse(json);
+    } else {
+        blob = Second.new_game_blob;
+    }
+    return new Second(blob);
+
+};
+
